04591ab4fe109f8c1c8411ca77422b4f0fc332bd
mkulak <kleshney@gmail.com>
2020-05-18 23:28:16 +0200

    add sqlite

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use hyper::http::Version;
 use bytes::{Bytes};
+use rusqlite::{Connection, Result};
+use rusqlite::NO_PARAMS;
+use std::collections::HashMap;
 
 trait ToString {
     fn to_str(&self) -> &'static str;
@@ -35,10 +38,10 @@ async fn routes(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     });
     println!();
 
-    match (method, uri.path()) {
-        (Method::GET, "/") => Ok(Response::new(Body::from("Hello"))),
-        (Method::POST, "/notifications") => Ok(Response::new(Body::from("OK"))),
-        (Method::GET, "/notification-results") => {
+    match uri.path() {
+        "/" => Ok(Response::new(Body::from("Hello"))),
+        "/notifications" => Ok(Response::new(Body::from("OK"))),
+        "/notification-results" if method == Method::GET => {
             // let chunk_stream = whole_body.map_ok(|chunk| {
             //     chunk
             //         .iter()
@@ -49,7 +52,7 @@ async fn routes(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
             Ok(Response::new(Body::from("OK")))
         }
 
-        (Method::POST, "/echo/reversed") => {
+        "/echo/reversed" if method == Method::POST => {
             let reversed_body = whole_body.iter().rev().cloned().collect::<Vec<u8>>();
             Ok(Response::new(Body::from(reversed_body)))
         }
@@ -63,12 +66,76 @@ async fn routes(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     }
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = ([127, 0, 0, 1], 3000).into();
-    let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(routes)) });
-    let server = Server::bind(&addr).serve(service);
-    println!("Listening on http://{}", addr);
-    server.await?;
+// #[tokio::main]
+// async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+//     let addr = ([127, 0, 0, 1], 3000).into();
+//     let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(routes)) });
+//     let server = Server::bind(&addr).serve(service);
+//     println!("Listening on http://{}", addr);
+//     server.await?;
+//     Ok(())
+// }
+
+#[derive(Debug)]
+struct Cat {
+    name: String,
+    color: String,
+}
+
+fn main() -> Result<()> {
+    let conn = Connection::open("cats.db")?;
+
+    conn.execute(
+        "create table if not exists cat_colors (
+             id integer primary key,
+             name text not null unique
+         )",
+        NO_PARAMS,
+    )?;
+    conn.execute(
+        "create table if not exists cats (
+             id integer primary key,
+             name text not null,
+             color_id integer not null references cat_colors(id)
+         )",
+        NO_PARAMS,
+    )?;
+
+    let mut cat_colors = HashMap::new();
+    cat_colors.insert(String::from("Blue"), vec!["Tigger", "Sammy"]);
+    cat_colors.insert(String::from("Black"), vec!["Oreo", "Biscuit"]);
+
+    for (color, catnames) in &cat_colors {
+        conn.execute(
+            "INSERT INTO cat_colors (name) values (?1)",
+            &[&color.to_string()],
+        )?;
+        let last_id: String = conn.last_insert_rowid().to_string();
+
+        for cat in catnames {
+            conn.execute(
+                "INSERT INTO cats (name, color_id) values (?1, ?2)",
+                &[&cat.to_string(), &last_id],
+            )?;
+        }
+    }
+    let mut stmt = conn.prepare(
+        "SELECT c.name, cc.name from cats c
+         INNER JOIN cat_colors cc
+         ON cc.id = c.color_id;",
+    )?;
+
+    let cats = stmt.query_map(NO_PARAMS, |row| {
+        Ok(Cat {
+            name: row.get(0)?,
+            color: row.get(1)?,
+        })
+    })?;
+
+    for cat in cats {
+        println!("Found cat {:?}", cat);
+    }
+
+
     Ok(())
 }
