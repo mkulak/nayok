232d70606fb6a882beb549488918bbb9d6fab1a9
mkulak <kleshney@gmail.com>
2020-05-23 13:14:18 +0200

    save headers (wip), cleanup

--- src/main.rs
+++ src/main.rs
@@ -10,41 +10,36 @@ use std::collections::HashMap;
 use base64;
 use chrono::{DateTime, NaiveDateTime, Utc};
 
-trait ToString {
-    fn to_str(&self) -> &'static str;
-}
-
-impl ToString for Version {
-    fn to_str(&self) -> &'static str {
-        match *self {
-            Version::HTTP_10 => "HTTP 1.0",
-            Version::HTTP_11 => "HTTP 1.1",
-            Version::HTTP_2 => "HTTP 2.0",
-            Version::HTTP_3 => "HTTP 3.0",
-            _ => panic!("Unknown http version")
-        }
-    }
-}
-
 static SCHEMA_SQL: &'static str = include_str!("schema.sql");
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = ([127, 0, 0, 1], 3000).into();
+    let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(routes)) });
+    let server = Server::bind(&addr).serve(service);
+    println!("Listening on http://{}", addr);
+    server.await?;
+    Ok(())
+}
 
 async fn save_notification(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     let method = req.method().clone();
     let uri = req.uri().path_and_query().unwrap().clone();
-    // println!("{} {}", method.as_str(), uri);
-    // req.headers().iter().for_each(|h| {
-    //     println!("{}: {}", h.0.as_str(), h.1.to_str().unwrap());
-    // });
-    let headers = "";
+    let mut headers = String::new();
+    req.headers().iter().for_each(|h| {
+        let res = format!("{}:{};", h.0.as_str(), h.1.to_str().unwrap());
+        headers.push_str(res.as_str())
+    });
+
     let body = req.into_body();
-    let body_bytes: Bytes = hyper::body::to_bytes(body).await?;
+    let body_bytes = hyper::body::to_bytes(body).await?;
     let body_vector = body_bytes.iter().cloned().collect::<Vec<u8>>();
     let body_base64 = base64::encode(&body_vector[..]);
 
     let conn = Connection::open("events.db").unwrap();
     conn.execute(
         "INSERT INTO events (relative_uri, method, headers, body) values (?1, ?2, ?3, ?4)",
-        &[uri.as_str(), method.as_str(), headers, body_base64.as_str()],
+        &[uri.as_str(), method.as_str(), headers.as_str(), body_base64.as_str()],
     ).unwrap();
     let last_id = conn.last_insert_rowid();
     println!("last_id: {}", last_id);
@@ -69,37 +64,8 @@ fn not_found() -> Result<Response<Body>, hyper::Error> {
     Ok(not_found)
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = ([127, 0, 0, 1], 3000).into();
-    let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(routes)) });
-    let server = Server::bind(&addr).serve(service);
-    println!("Listening on http://{}", addr);
-    server.await?;
-    Ok(())
-}
-
-#[derive(Debug)]
-struct Cat {
-    name: String,
-    color: String,
-}
-
 fn main2() -> Result<()> {
     let conn = Connection::open("events.db")?;
-    // conn.execute(SCHEMA_SQL, NO_PARAMS)?;
-    // let headers = "Foo: Bar;Bax: Boo";
-    // let method = "GET";
-    // let relative_uri = "/p1/p2?blah=bom";
-    // let body = base64::encode(&vec![1, 2, 3][..]);
-    // conn.execute(
-    //     "INSERT INTO events (relative_uri, method, headers, body) values (?1, ?2, ?3, ?4)",
-    //     &[&relative_uri, &method, &headers, body.as_str()],
-    // )?;
-    // let last_id = conn.last_insert_rowid();
-    // println!("last_id: {}", last_id);
-
-
     let mut stmt = conn.prepare(
         "SELECT id, relative_uri, method, headers, body, created_at from events WHERE id > ?1 AND created_at > ?2"
     )?;
