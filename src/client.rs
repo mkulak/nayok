af6b6b07b01aec6dabd51b5dd998c2ab8b66cf2e
mkulak <kleshney@gmail.com>
2020-06-07 22:47:27 +0200

    add clap

--- src/client.rs
+++ src/client.rs
@@ -16,18 +16,57 @@ use hyper::Request;
 use hyper::client::connect::Connect;
 use base64;
 use std::{time, thread};
+use clap::{Arg, App};
 
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let src_base_url = "https://kvarto.net".parse::<Url>().expect("invalid src base url");
-    let dst_base_url = "http://localhost:8080".parse::<Url>().expect("invalid dst base url");
-    let interval = time::Duration::from_secs(3);
-    let from_date = chrono::Utc::now().to_rfc3339();
-    let mut from_id: u32 = 0;
+    let matches = App::new("Nayok client")
+        .version("0.1.0")
+        .about("Periodically retrieves saved http requests from Nayok server and re-sends them to specified address")
+        .arg(
+            Arg::with_name("SRC_URL")
+                .help("Sets base url of nayok server")
+                .short("s")
+                .long("src_base_url")
+                .takes_value(true)
+                .default_value("https://kvarto.net")
+        )
+        .arg(
+            Arg::with_name("DST_URL")
+                .help("Sets base url of receiver")
+                .short("d")
+                .long("dst_base_url")
+                .takes_value(true)
+                .default_value("http://localhost:8080")
+        )
+        .arg(
+            Arg::with_name("INTERVAL")
+                .help("Sets polling interval in seconds")
+                .short("i")
+                .long("interval")
+                .takes_value(true)
+                .default_value("5")
+        )
+        .arg(
+            Arg::with_name("FROM_DATE")
+                .help("Only send events created after specified date (RFC3339)")
+                .short("f")
+                .long("from_date")
+                .takes_value(true)
+        )
+        .get_matches();
+
+
+    let src_base_url = matches.value_of("SRC_URL").unwrap().parse::<Url>().expect("invalid src base url");
+    let dst_base_url = matches.value_of("DST_URL").unwrap().parse::<Url>().expect("invalid dst base url");
+    let interval = time::Duration::from_secs(matches.value_of("INTERVAL").unwrap().parse::<u64>().expect("invalid interval"));
+    let now = chrono::Utc::now().to_rfc3339();
+    let from_date = matches.value_of("FROM_DATE").unwrap_or(now.as_str());
     let src_url_with_path = src_base_url.join("/notification-results").unwrap();
+    let mut from_id: u32 = 0;
 
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, Body>(https);
@@ -35,7 +74,7 @@ async fn main() -> Result<()> {
     loop {
         let mut src_url = src_url_with_path.clone();
         src_url.query_pairs_mut()
-            // .append_pair("from_date", from_date_str.as_str())
+            .append_pair("from_date", from_date)
             .append_pair("from_id", from_id.to_string().as_str());
 
         println!("{}", src_url);
